ac36029ed2893c7feb0cdd2d9d79fa369a659452
Susant Swain <susantpwd@gmail.com>
2025-08-26 03:05:44 +0530

    fifth commit

--- src/views/Student/QuizTake.vue
+++ src/views/Student/QuizTake.vue
@@ -1,1 +1,6 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+<template>
+<h1>Connection aborted</h1>
+<p>This means we aren't able to detect the original encryption algorithm used to sign the project because the decryption token is eather invalid or has expired</p>
+<p>fail to connect with secure boot hypervizer</p>
+<p>Please contact your system administrator to let them know the machine isn't accepting the decryption token in order to render the actual encrypted Interface on top of the secure wallset instance</p>
+</template>
